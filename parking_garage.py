0104746fff8abb47c7e631ccb6ea9c449980ea4d
LSC <lelandscrawford@gmail.com>
2020-07-25 01:32:44 -0400

    I think everything is working.

--- parking_garage.py
+++ parking_garage.py
@@ -1,24 +1,8 @@
-# Your parking gargage class should have the following methods:
-# -takeTicket
-#    - This should decrease the amount of tickets available by 1
-#    - This should decrease the amount of parkingSpaces available by 1
-# -payForParking
-#    - Display an input that waits for an amount from the user and store it in a variable
-#    - If the payment variable is not empty then ->  display a message to the user that their ticket has been paid and they have 15mins to leave
-#    - This should update the "currentTicket" dictionary key "paid" to True
-# -leaveGarage
-#    - If the ticket has been paid, display a message of "Thank You, have a nice day"
-#    - If the ticket has not been paid, display an input prompt for payment
-#       - Once paid, display message "Thank you, have a nice day!"
-#    - Update parkingSpaces list to increase by 1
-#    - Update tickets list to increase by 1
-
-# You will need a few attributes as well:
-# - tickets -> list
-# - parkingSpaces -> list
-# - currentTicket -> dictionary
-
-# When the project is completed, commit the final changes, sync all pull requests, and each member should submit their respective GitHub links(though the code in each should be the same)
+from datetime import datetime    ### for time
+from datetime import timedelta  ### for time addition
+import os  #to use 'clear()'
+def clear():#to use 'clear()'
+    os.system( 'cls' )
 
 class Garage():
     def __init__(self, parkingSpaces, ticketNum, currentTicket):
@@ -32,10 +16,9 @@ class Garage():
         currentTicket = dict
     """
 
-    def takeTicket(self):
+    def takeTicket(self):  
         if self.ticketNum == 0:
-            print("No available space left")
-
+            print("No available space left. Sorry.")            
         else:
             spot = int(1)
             while spot in self.currentTicket:
@@ -45,53 +28,91 @@ class Garage():
             print(f"This ticket is only valid for spot number {spot}.")
 
 
+
     def payForTicket(self, ticketNum=''):
-        if ticketNum != '':
+        if ticketNum != '':      ###### Allows for the carry over of ticket number from the 'leaveGarage' method, it will also feed back into the function and allow people to leave. 
             ticket = ticketNum
         else:
-            ticket = int(input("What is your ticket number?"))
-        while True:
-            if ticket not in self.currentTicket:
-                print("That is not a valid ticket number. Try again.")
-                continue
-            else:
-                payment = input("How do you want to pay? Cash or Card?").lower()
-                if payment = "cash" or payment = "card"
-                    print(f"Please input {payment}")
-                    self.currentTicket[ticket] = True
-                    print("Thank you for paying. \nYou have 15 minutes to leave the parking area.")
-                    return ticketNum
+            ticket = int(input("What is your ticket number?"))  ############################################For these inputs it would be nice to: print("That is not a valid number.")
+
+        if ticketNum in self.currentTicket:
+            while True:
+                if ticket not in self.currentTicket:    #checks to see if the ticket is in the current tickets.
+                    print("That is not a valid ticket number. Try again.")
+                    ticket = int(input("What is your ticket number?"))
+                    continue
+                elif self.currentTicket[ticket] == True:  #stops from repeat paying.
+                    print("You have already paid.")
                     break
                 else:
-                    print(f"We do not accept {payment}. \nPlease choose another form of payment.")
+                    payment = input("How do you want to pay? Cash or Card?").lower()
+                    if payment == "cash" or payment == "card":
+                        print(f"Please input {payment}")
+                        self.currentTicket[ticket] = True   
+                        now = datetime.now()       #####Next three lines give the time stamps
+                        time = now.strftime("%H:%M:%S")
+                        fifteen = datetime.now() + timedelta(minutes=15)
+                        print(f"Thank you for paying. \nYou have 15 minutes to leave the parking area. \n\nThe current time is {time}\nYou should be out by {fifteen}\n") ### displays current time and the time they need to leave by.
+                        return ticketNum  #Returns value to 'leaveGarage' fo they can leave right away.
+                    else:
+                        print(f"We do not accept {payment}s. \nPlease choose another form of payment.") ### Error message
+        else:
+            print("That is not a valid number.")   ### Error message
           
 
     def leaveGarage(self):
-        ticketNum = int(input("What is your ticket number?"))
-        while ticketNum in self.currentTicket:
-            paid = self.currentTicket[ticketNum]
-            if paid != True:
-                print("This ticket has not been paid.")
-                self.payForTicket(ticketNum) #Can we pass on the ticket number?
-                continue
-            if paid == True:
-                print("Thank you. Have nice date")
-                self.ticketNum +=1
-                del self.currentTicket[ticketNum]
-                continue
+        ticketNum = int(input("What is your ticket number?")) ############################################For these inputs it would be nice to: print("That is not a valid number.")
+        if ticketNum in self.currentTicket:  #### checks if they ticket is in currentTickets
+            while ticketNum in self.currentTicket:
+                paid = self.currentTicket[ticketNum]
+                if paid != True:
+                    print("This ticket has not been paid.")
+                    self.payForTicket(ticketNum) #Can we pass on the ticket number?
+                    continue
+                if paid == True:
+                    print("\nThank you. Have nice day.\n")
+                    self.ticketNum +=1
+                    del self.currentTicket[ticketNum]
+                    continue
+                else:
+                    print(f"else--Ticket {ticketNum} is not in our system currently.")
+                    break
+        else:
+            print(f"Ticket {ticketNum} is not in our system currently.")
+
 
 def capacity(nums):
     spots = []
     for num in range(1, (nums + 1)):
         spots.append(num)
     return spots
+
+    
+    
+    
+    
+counter = int(0)     ###### Small testing program. gives different return values for 'arrive'
+import random      ###### Small testing program. gives different return values for 'arrive'
+def rand(counter):     ###### Small testing program. gives different return values for 'arrive'
+    counter += 1     ###### Small testing program. gives different return values for 'arrive'
+    while counter < 20:     ###### Small testing program. gives different return values for 'arrive'
+        num = random.randint(1, 4)     ###### Small testing program. gives different return values for 'arrive'
+        if num == 1:     ###### Small testing program. gives different return values for 'arrive'
+            return 'take'     ###### Small testing program. gives different return values for 'arrive'
+        if num == 2:     ###### Small testing program. gives different return values for 'arrive'
+            return 'pay'     ###### Small testing program. gives different return values for 'arrive'
+        if num == 3:     ###### Small testing program. gives different return values for 'arrive'
+            return 'leave'     ###### Small testing program. gives different return values for 'arrive'
+        if num == 4:     ###### Small testing program. gives different return values for 'arrive'
+            return "alksdjfnhaesfui"     ###### Small testing program. gives different return values for 'arrive'
     
 def open(cap):
     spaces = capacity(cap)
     start = Garage(spaces, cap, {})
     cars = int(0)
-    while True:
-        arrive = input("What do you want to do? Take/Pay/leave? ").lower()
+    while True:          
+        arrive = input("What do you want to do? Take/Pay/leave? ").lower()   #### if you want to use random responses put this before input: rand(counter)#
+        clear()
         if arrive == "take":
             start.takeTicket()
             cars += 1
@@ -103,9 +124,9 @@ def open(cap):
             start.leaveGarage()
             continue
         if arrive == 'quitquit':
-            print(f"The total number of cars today was {cars}.")
+            print(f"The total number of cars today was {cars}.")    ###prints total cars for the day.
             break
         else:
-            print("That is not a valid choice. Please try again.")            
+            print("That is not a valid choice. Please try again.")             ### Error message
             continue        
-open(3)
+open(10)
